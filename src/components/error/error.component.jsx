ed109cec4da524d4e754aeb6993956489de46fb8
oliaboria <olgaboris94@gmail.com>
2020-06-30 19:40:55 +0300

    rename error -> errorMessage

--- src/components/error/error.component.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import './_error.scss';
-
-const Error = () => <h2 className="error-header">Something went wrong.</h2>;
-
-export default Error;
---
+++ src/components/errorMessage/errorMessage.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './_errorMessage.scss';
+
+const ErrorMessage = () => (
+    <h2 className="error-header">
+        Ops...Something went wrong. Please try again later
+    </h2>
+);
+
+export default ErrorMessage;
